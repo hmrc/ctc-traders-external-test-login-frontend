151701d77d27759c0ad8c4d1233229b2af62469b
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-07-04 07:15:55 +0100

    CTCP-5506: update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "9.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.3.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion
   )
