ed7eb9e0e21d00a53926688ee2e5abfc68a9278f
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-07-07 14:43:56 +0100

    CTCP-6571: Dependency updates.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,22 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.6.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.7.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalatest"           %% "scalatest"               % "3.2.19",
-    "org.mockito"             %  "mockito-core"            % "5.14.2",
+    "org.mockito"             %  "mockito-core"            % "5.18.0",
     "org.scalatestplus"       %% "mockito-5-12"            % "3.2.19.0",
     "org.scalacheck"          %% "scalacheck"              % "1.18.1",
     "org.scalatestplus"       %% "scalacheck-1-18"         % "3.2.19.0",
-    "org.jsoup"               %  "jsoup"                   % "1.18.1"
+    "org.jsoup"               %  "jsoup"                   % "1.21.1"
   ).map(_ % "test")
 
   def apply(): Seq[ModuleID] = compile ++ test
